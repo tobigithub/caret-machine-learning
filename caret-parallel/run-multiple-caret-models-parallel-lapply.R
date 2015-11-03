3a1ac9c3df20da76dbfad547675f77456789cde0
Tobias Kind <tkind@ucdavis.edu>
2015-11-03 15:02:47 -0800

    Update and rename  run-multiple-caret-models-parallel-lapply.R to run-multiple-caret-models-parallel-lapply.R

--- caret-parallel/run-multiple-caret-models-parallel-lapply.R
+++ caret-parallel/run-multiple-caret-models-parallel-lapply.R
@@ -3,9 +3,8 @@
 # Tobias Kind (2015)
 
 
-require(caret); data(BloodBrain); m <- c("qrf","xgbTree","knn","rf");
-library(doParallel); cl <- makeCluster(8); registerDoParallel(cl)
 require(caret); data(BloodBrain); m <- c("qrf","xgbTree","rknn","knn","rf");
+library(doParallel); cl <- makeCluster(8); registerDoParallel(cl)
 t2 <- lapply(m,function(x) {set.seed(123); seeds <- vector(mode = "list", length = nrow(bbbDescr) + 1); seeds <- lapply(seeds, function(x) 1:20); t1 <- train(bbbDescr, logBBB, (x),trControl = trainControl(method = "cv",seeds=seeds))})
 r2 <- lapply(1:length(t2), function(x) {cat(sprintf("%-10s",(m[x])));cat(t2[[x]]$results$Rsquared[which.min(t2[[x]]$results$RMSE)],"\t"); cat(t2[[x]]$results$RMSE[which.min(t2[[x]]$results$RMSE)],"\n")})
 stopCluster(cl); registerDoSEQ();
