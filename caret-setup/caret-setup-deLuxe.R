bf97560557c6c7d5209fd49ea4bd7a63744f768d
Tobias Kind <tkind@ucdavis.edu>
2015-11-05 09:41:33 -0800

    Update caret-setup-deLuxe.R

--- caret-setup/caret-setup-deLuxe.R
+++ caret-setup/caret-setup-deLuxe.R
@@ -1,23 +1,24 @@
 # Installation of caret package with allmost 400 required caret dependencies 
 # https://github.com/tobigithub/caret-machine-learning
 # Tobias Kind (2015)
+
+# 1) load few caret packages from BioConductor, this will create most troubles
+# this is a static solution (not good) check with below URL for more info
+# https://github.com/topepo/caret/blob/master/release_process/update_pkgs.R
+source("https://bioconductor.org/biocLite.R")
+biocLite()
+biocLite(c("arm", "gpls", "logicFS", "vbmp"))
  
 # installs most of the 340 caret dependencies + seven commonly used but not all of them
 mostCommon <- c("caret", "AppliedPredictiveModeling", "ggplot2", "data.table", "plyr", "knitr", "shiny", "xts", "lattice")
 install.packages(mostCommon, dependencies = c("Imports", "Depends", "Suggests"))          
 
 # then load caret and check which additional libraries covering 200 models need to be installed
-# warnings will still exist
+# warnings will still exist; because caret loaded few dependencies they can not be updated
+# during runtime, may create errors
 require(caret); sessionInfo();
 caretLibs <- unique(unlist(lapply(getModelInfo(), function(x) x$library)))
 install.packages(caretLibs, dependencies = c("Imports", "Depends", "Suggests")) 
-
-# now load caret packages from BioConductor, this will create most troubles
-# this is a static solution (not good) check with below URL for more info
-# https://github.com/topepo/caret/blob/master/release_process/update_pkgs.R
-source("https://bioconductor.org/biocLite.R")
-biocLite()
-biocLite(c("arm", "gpls", "logicFS", "vbmp"))
 
 # "Warning: cannot remove prior installation of package"
 # in case of final installation issues, check packages plyr, MASS and ggplot2
