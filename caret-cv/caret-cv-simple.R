2c3d2333bc5031a8159acddfc0e885dfaad083c8
Tobias Kind <tkind@ucdavis.edu>
2015-11-05 15:14:39 -0800

    Update caret-cv-simple.R

--- caret-cv/caret-cv-simple.R
+++ caret-cv/caret-cv-simple.R
@@ -11,12 +11,5 @@
   tc <- trainControl(method="boot")
   fit1 <- train(bbbDescr, logBBB, trControl=tc, method="knn");  fit1
   
-# All available six cross-validation methods applied  
-  require(caret); data(BloodBrain); 
-  cvMethods <- c("boot632","LGOCV","LOOCV","cv","repeatedcv", "boot" );
-  all <- sapply(cvMethods ,function(x) {set.seed(123); print(x); tc <- trainControl(method=(x))
-                    fit1 <- train(bbbDescr, logBBB, trControl=tc, method="knn") }); all 
-  all[4, ]
 
-  
 ### END
