39e8e81129a23be56fa7da369024118196516004
Tobias Kind <tkind@ucdavis.edu>
2015-11-05 13:54:43 -0800

    Update caret-cv-simple.R

--- caret-cv/caret-cv-simple.R
+++ caret-cv/caret-cv-simple.R
@@ -1,3 +1,7 @@
+# Run simple cross-validation method with caret and knn
+# https://github.com/tobigithub/caret-machine-learning
+# Tobias Kind (2015)
+
 # Single example, no cross-validation
   require(caret); data(BloodBrain); set.seed(123);
   fit1 <- train(bbbDescr, logBBB, "knn"); fit1
@@ -7,3 +11,4 @@
   tc <- trainControl(method="boot")
   fit1 <- train(bbbDescr, logBBB, trControl=tc, method="knn");  fit1
   
+### END
