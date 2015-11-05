b32c469363e07635a2d268b2fb7dc152b6d25023
Tobias Kind <tkind@ucdavis.edu>
2015-11-04 17:44:44 -0800

    Create caret-setup-comfort.R

---
+++ caret-setup/caret-setup-comfort.R
@@ -0,0 +1,9 @@
+# Installation of caret package with commonly used 340 dependencies
+# https://github.com/tobigithub/caret-machine-learning
+# Tobias Kind (2015)
+
+# installs most of the 340 caret dependencies + seven commonly used but not all of them
+mostCommon <- c("caret", "ggplot2", "data.table", "plyr", "knitr", "shiny", "xts", "lattice")
+install.packages(mostCommon, dependencies = c("Imports", "Depends", "Suggests"))          
+
+### END
