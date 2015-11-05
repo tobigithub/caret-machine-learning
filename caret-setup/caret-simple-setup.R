eae2fe046e9cca06c4aa797913b1414a70de5506
Tobias Kind <tkind@ucdavis.edu>
2015-11-04 17:27:13 -0800

    Update caret-simple-setup.R

--- caret-setup/caret-simple-setup.R
+++ caret-setup/caret-simple-setup.R
@@ -1,1 +1,6 @@
+# Run multiple caret models in parallel using lapply
+# https://github.com/tobigithub/caret-machine-learning
+# Tobias Kind (2015)
+
+# installs most of the 300 caret dependencies  but not all of them
 install.packages("caret", dependencies = c("Imports", "Depends", "Suggests"))
