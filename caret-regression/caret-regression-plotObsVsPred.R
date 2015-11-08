e4ec61aea922a281ff8b21bbd4d1bd049fc3855b
Tobias Kind <tkind@ucdavis.edu>
2015-11-07 23:02:40 -0800

    Create caret-regression-plotObsVsPred.R

---
+++ caret-regression/caret-regression-plotObsVsPred.R
@@ -0,0 +1,30 @@
+Source: http://www.inside-r.org/packages/cran/caret/docs/plotObsVsPred
+
+
+# load libraries and models
+require(caret)
+require(mlbench)
+data(BostonHousing)
+
+# perform CART (Classification And Regression Tree) analysis
+set.seed(123)
+rpartFit <- train(BostonHousing[1:100, -c(4, 14)], 
+                  BostonHousing$medv[1:100], 
+                  "rpart", tuneLength = 9)
+
+# perform PLS (Partial Least Squares) analysis
+set.seed(123)
+plsFit <- train(BostonHousing[1:100, -c(4, 14)], 
+                BostonHousing$medv[1:100], 
+                "pls")
+
+# extract optimal tuning values for further use
+predVals <- extractPrediction(list(rpartFit, plsFit), 
+                              testX = BostonHousing[101:200, -c(4, 14)], 
+                              testY = BostonHousing$medv[101:200], 
+                              unkX = BostonHousing[201:300, -c(4, 14)])
+
+# plot CART and PLS observed vs predicted values for training and test set
+plotObsVsPred(predVals)
+
+### END
