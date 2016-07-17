2d62cdaedc7290df18773ae0665e3d466ca45555
Tobias Kind <tkind@ucdavis.edu>
2016-07-16 18:56:15 -0700

    Update caret-parallel-train-rf-deLuxe.R

--- caret-parallel/caret-parallel-train-rf-deLuxe.R
+++ caret-parallel/caret-parallel-train-rf-deLuxe.R
@@ -25,3 +25,19 @@ fit1;
 stopCluster(cl)
 registerDoSEQ()
 ### END
+
+# 208 samples
+# 134 predictors
+# 
+# No pre-processing
+# Resampling: Bootstrapped (25 reps) 
+# Summary of sample sizes: 208, 208, 208, 208, 208, 208, ... 
+# Resampling results across tuning parameters:
+# 
+#   mtry  RMSE       Rsquared 
+#     2   0.5443770  0.5725600
+#    68   0.5408819  0.5568365
+#   134   0.5490382  0.5413179
+# 
+# RMSE was used to select the optimal model using  the smallest value.
+# The final value used for the model was mtry = 68. 
